4d22558da48e4b95e2f771d9abc295f20a0d717f
mhasan8801 <hasanmuhammad197@gmail.com>
2025-06-21 10:54:07 +0700

    Atomicity concept

--- acid_concept.sql
+++ acid_concept.sql
@@ -18,6 +18,21 @@ MODIFY id VARCHAR(100) NOT NULL;
 
 SELECT * FROM accounts;
 
+-- 1. ATOMICITY : 
+-- Transaksi berhasil = semua statement berhasil
+START TRANSACTION;
+INSERT INTO accounts(id, name, balace)
+VALUES ('hasan', 'M. Hasan', 5000000);
+INSERT INTO accounts(id, name, balace)
+VALUES ('sans', 'Sanstuy', 1000000);
+COMMIT; -- Kedua data baru berhasil ditambahkan disini karena semua statement berhasil
+
+-- Transaksi gagal = semua statement != berhasil / digagalkan
+START TRANSACTION;
+DELETE FROM accounts WHERE id = "hasan";
+DELETE FROM accounts WHERE id = "sans";
+ROLLBACK; -- Kedua data gagal dihapus disini karena dirollback
+
 
 
 
